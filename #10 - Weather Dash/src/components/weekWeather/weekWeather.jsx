c2d1ef41405eeb223464a6964b1272e086d8a444
Jhonata Escatolini <jescatolini@gmail.com>
2023-03-21 18:31:02 -0300

    JSX :thermometer:

--- #10 - Weather Dash/src/components/weekWeather/weekWeather.jsx
+++ #10 - Weather Dash/src/components/weekWeather/weekWeather.jsx
@@ -10,7 +10,7 @@ export default function WeekWeather({
     maxWeeklyTemperature,
 }) {
     const weekDays = [
-        "Domingo",
+        "Domingo",  
         "Segunda",
         "Terça",
         "Quarta",
