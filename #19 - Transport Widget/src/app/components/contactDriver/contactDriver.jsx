ad2a2e40b9811cfcc102afac9717dddc7a8c194a
Jhonata Escatolini <jescatolini@gmail.com>
2023-05-19 14:58:54 -0300

    feat: add contactDriver.jsx

---
+++ #19 - Transport Widget/src/app/components/contactDriver/contactDriver.jsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import styles from "./contactDriver.module.css";
+
+import shield from "../../../../public/shieldIcon.svg";
+import phone from "../../../../public/phoneIcon.svg";
+
+export default function ContactDriver() {
+  return (
+    <div className={styles.container}>
+      <input
+        className={styles.inputSendMessage}
+        type="text"
+        name="sendMessage"
+        id="sendMessage"
+        placeholder="Enviar mensagem para Boris..."
+      />
+      <div className={styles.emergencyIcons}>
+        <Image
+          className={styles.phone}
+          src={phone}
+          alt="icon of a phone"
+          width={48}
+          height={48}
+        />
+        <Image
+          className={styles.shield}
+          src={shield}
+          alt="icon of a shield"
+          width={46}
+          height={48}
+        />
+      </div>
+    </div>
+  );
+}
